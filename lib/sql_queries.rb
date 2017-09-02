602f0ea82244cd394c149e10449efcc72c39f933
Jordan Johnson <jkj86@hotmail.com>
2017-09-02 00:26:27 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,9 +23,9 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(name) FROM bears where temperament='GOOFY'"
+  "SELECT COUNT(*) FROM bears where temperament='GOOFY'"
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT NAME from bears where name = NULL"
+  "SELECT NAME from bears where name IS NULL"
 end
