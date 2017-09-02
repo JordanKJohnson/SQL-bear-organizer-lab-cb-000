95307284ae3d9f62d462e38b7de5ef058e5a725c
Jordan Johnson <jkj86@hotmail.com>
2017-09-02 00:06:56 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,15 @@
-INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ()
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. Chocolate','13','M','BROWN','DOCILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. Chocolate','13','M','BROWN','DOCILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. Chocolate','13','M','BROWN','DOCILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. Chocolate','13','M','BROWN','DOCILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. Chocolate','13','M','BROWN','DOCILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. Chocolate','13','M','BROWN','DOCILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. Chocolate','13','M','BROWN','DOCILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('8',NULL,'11','F','BROWN','HOSTILE','T');
