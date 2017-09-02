681382741fb4abd386504dafb42cff4ccf322bdb
Jordan Johnson <jkj86@hotmail.com>
2017-09-02 00:08:12 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,8 +8,8 @@ INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. 
 
 INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. Chocolate','13','M','BROWN','DOCILE','T');
 
-INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. Chocolate','13','M','BROWN','DOCILE','T');
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('6','Grinch','13','M','BROWN','HOSTILE','T');
 
-INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. Chocolate','13','M','BROWN','DOCILE','T');
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('7','Wendy','3','F','BROWN','DOCILE','T');
 
 INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('8',NULL,'11','F','BROWN','HOSTILE','T');
