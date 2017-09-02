60bb8ce117d9cd24f70b512caf594e866974fdf8
Jordan Johnson <jkj86@hotmail.com>
2017-09-02 00:34:33 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('1','Mr. Chocolate','13','M','BROWN','DOCILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('2','Rowdy','1','M','BROWN','HOSTILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('3','Tabitha','23','F','BROWNISH-RED','DOCILE','F');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('4','Sergeant Brown','13','M','BROWN','DOCILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('5','Melissa','13','F','BROWN','DOCILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('6','Grinch','13','M','BROWN','GOOFY','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('7','Wendy','3','F','BROWN','DOCILE','T');
+
+INSERT INTO bears (id,name,age,gender,color,temperament,alive) VALUES ('8',NULL,'11','F','BROWN','HOSTILE','T');
