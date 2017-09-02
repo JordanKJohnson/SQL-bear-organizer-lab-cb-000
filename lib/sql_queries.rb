6e6956129b0617865266567630a7b69671350724
Jordan Johnson <jkj86@hotmail.com>
2017-09-02 00:27:47 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT NAME from bears where name IS NULL"
+  "SELECT ID from bears where name IS NULL"
 end
